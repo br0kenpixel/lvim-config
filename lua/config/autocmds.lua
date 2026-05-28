cbeb720c4be09dbb0d2c5aa4af7cd51acfca7e5e
Fábián Varga <br0kenpixel@varganet.org>
2026-05-28 10:44:11 +0200

    auto hover docs

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,26 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.opt.updatetime = 3000
+
+vim.api.nvim_create_autocmd("CursorHold", {
+  callback = function()
+    local ignored_filetypes = {
+      snacks_dashboard = true,
+      lazy = true,
+      mason = true,
+      help = true,
+    }
+
+    if ignored_filetypes[vim.bo.filetype] then
+      return
+    end
+
+    if #vim.lsp.get_clients({ bufnr = 0 }) == 0 then
+      return
+    end
+
+    vim.lsp.buf.hover()
+  end,
+})
