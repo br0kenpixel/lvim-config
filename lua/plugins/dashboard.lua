bdf8fd759dd146cc51fcdeb7e9c2e438b5f8ee6a
Fábián Varga <br0kenpixel@varganet.org>
2026-05-28 10:30:34 +0200

    cleanup home screen options

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -3,10 +3,45 @@ return {
     "folke/snacks.nvim",
     opts = {
       dashboard = {
-        -- remove the LazyVim logo/header entirely
         sections = {
           { section = "keys", gap = 1, padding = 1 },
-          { section = "startup" },
+          --Disable the stats text
+          --{ section = "startup" },
+        },
+
+        preset = {
+          keys = {
+            {
+              icon = " ",
+              key = "n",
+              desc = "New File",
+              action = ":ene | startinsert",
+            },
+            {
+              icon = " ",
+              key = "c",
+              desc = "Config",
+              action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
+            },
+            {
+              icon = " ",
+              key = "x",
+              desc = "Lazy Extras",
+              action = ":LazyExtras",
+            },
+            {
+              icon = "󰒲 ",
+              key = "l",
+              desc = "Lazy",
+              action = ":Lazy",
+            },
+            {
+              icon = " ",
+              key = "q",
+              desc = "Quit",
+              action = ":qa",
+            },
+          },
         },
       },
     },
